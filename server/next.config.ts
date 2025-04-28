f1dd478410a3722b1cea35d63ccee7647ee13a42
Joseph Sachs <jhalesachs@gmail.com>
2025-04-28 16:08:12 -0400

    Standard static resources from fresh project

--- server/next.config.ts
+++ server/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  reactStrictMode: true,   // React Strict Mode (helpful during development)
-  
-  // No special static generation or page directory configurations
-  images: {
-    domains: []  // Disable any automatic image optimization if not required
-  }
+  /* config options here */
 };
-
 
 export default nextConfig;
